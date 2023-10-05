9016fd95e2879619deb1195ff35374c6addb5ccf
Your Name <you@example.com>
2023-10-05 16:14:04 +0200

    Add: plik testowy

---
+++ src/main/java/com/example/demo/Lab1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start application");
+
+		SpringApplication.run(Lab1Application.class, args);
+	}
+
+}
