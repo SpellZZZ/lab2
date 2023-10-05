d62012eae768f654e3eaba4e576e1be9e2081506
Your Name <you@example.com>
2023-10-05 16:26:40 +0200

    Add add -all

--- src/main/java/com/example/demo/Lab1Application.java
+++ src/main/java/com/example/demo/Lab1Application.java
@@ -9,6 +9,7 @@ public class Lab1Application {
 	public static void main(String[] args) {
 
 		System.out.println("Start application");
+		System.out.println("Info");
 
 		SpringApplication.run(Lab1Application.class, args);
 	}
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,4 @@
 package com.example.demo.controller;
 
-public class MainController {
+public class UserController {
 }
